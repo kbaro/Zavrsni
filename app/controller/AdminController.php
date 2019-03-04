7bc593063f9f022530f6422ef8841762924370c7
Kristijan Baro <baro.kristijan@gmail.com>
2019-03-04 15:49:58 +0100

    admin

--- app/controller/AdminController.php
+++ app/controller/AdminController.php
@@ -1,20 +1,15 @@
 <?php
-
 class AdminController{
     function singin(){
         $view = new View();
         $view->render('login',["message"=>""]);
     }
-
     function login()
     {
-
         $db=Db::getInstance();
         $expression = $db->prepare("select name,surname,email,password from operator where email=:email");
         $expression->execute(["email"=>Request::post("email")]);
-
         $view = new View();
-
         if($expression->rowCount()>0){
             $row=$expression->fetch();
             if(password_verify(Request::post("password"),$row->password)){
@@ -23,9 +18,7 @@ class AdminController{
                 $user->surname=$row->surname;
                 $user->email=$row->email;
                 $user->nameSurname=$row->name . " " . $row->surname;
-
                 Session::getInstance()->login($user);
-
                 $view->render('index',["message"=>"Welcome"]);
             }else{
                 $view->render('login',["message"=>"Email or password do not match"]);
@@ -33,20 +26,12 @@ class AdminController{
         }else{
                           $view->render('login',["message"=>"Wrong E-mail"]);
                              }
-
-
-
-
     //
-
   }
-
     function logout()
     {
-
         Session::getInstance()->logout();
         $view = new View();
         $view->render('index',["message"=>""]);
     }
 }
-
